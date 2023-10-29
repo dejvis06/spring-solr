6291cc404425b823ac0b702de3d8fba5edad46c9
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2023-10-29 15:21:01 +0100

    replace deprecated class

--- src/test/java/com/example/demo/SpringSolrApplicationTests.java
+++ src/test/java/com/example/demo/SpringSolrApplicationTests.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.beans.Field;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.impl.Http2SolrClient;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrInputDocument;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -17,45 +15,47 @@ import java.util.UUID;
 @SpringBootTest
 class SpringSolrApplicationTests {
 
-	final String solrUrl = "http://localhost:8983/solr";
+    final String solrUrl = "http://localhost:8983/solr";
 
-	@Test
+    @Test
     void indexBySolrInputDocument() throws SolrServerException, IOException {
-		final SolrClient client = getSolrClient();
-		final SolrInputDocument doc = new SolrInputDocument();
-		doc.addField("id", UUID.randomUUID().toString());
-		doc.addField("name", "Amazon Kindle Paperwhite");
+        final SolrClient client = getSolrClient();
+        final SolrInputDocument doc = new SolrInputDocument();
+        doc.addField("id", UUID.randomUUID().toString());
+        doc.addField("name", "Amazon Kindle Paperwhite");
 
-		final UpdateResponse updateResponse = client.add("solr_core", doc);
-		// Indexed documents must be committed
-		client.commit("solr_core");
+        final UpdateResponse updateResponse = client.add("solr_core", doc);
+        // Indexed documents must be committed
+        client.commit("solr_core");
     }
 
-	@Test
-	void indexByBean() throws SolrServerException, IOException {
-		final SolrClient client = getSolrClient();
-		final TechProduct kindle = new TechProduct("kindle-id-4", "Amazon Kindle Paperwhite");
-		final UpdateResponse response = client.addBean("solr_core", kindle);
+    @Test
+    void indexByBean() throws SolrServerException, IOException {
+        final SolrClient client = getSolrClient();
+        final TechProduct kindle = new TechProduct("kindle-id-4", "Amazon Kindle Paperwhite");
+        final UpdateResponse response = client.addBean("solr_core", kindle);
 
-		// Indexed documents must be committed
-		client.commit("solr_core");
-	}
+        // Indexed documents must be committed
+        client.commit("solr_core");
+    }
 
-	private HttpSolrClient getSolrClient() {
-		return new HttpSolrClient.Builder(solrUrl)
-				.withConnectionTimeout(10000)
-				.withSocketTimeout(60000)
-				.build();
-	}
+    private Http2SolrClient getSolrClient() {
+        return new Http2SolrClient.Builder(solrUrl)
+                .build();
+    }
 
-	private static class TechProduct {
-		@Field public String id;
-		@Field public String name;
+    private static class TechProduct {
+        @Field
+        public String id;
+        @Field
+        public String name;
 
-		public TechProduct(String id, String name) {
-			this.id = id;  this.name = name;
-		}
+        public TechProduct(String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
 
-		public TechProduct() {}
-	}
+        public TechProduct() {
+        }
+    }
 }
