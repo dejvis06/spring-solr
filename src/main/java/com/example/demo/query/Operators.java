2acfc561c0e42ae41033e425beaf55466b634dfc
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-12 11:43:00 +0100

    add operators

---
+++ src/main/java/com/example/demo/query/Operators.java
@@ -0,0 +1,32 @@
+package com.example.demo.query;
+
+public enum Operators {
+    AND("AND"),
+    OR("OR"),
+    IS("{field}:{value}"),
+    NOT("-{field}:"),
+    IS_NULL("-{field}:[* TO *]"),
+    IS_NOT_NULL("{field}:[* TO *]"),
+    BETWEEN("{field}:[{from} TO {to}]"),
+    LESS_THAN("{field}:[* TO {to}}"),
+    LESS_THAN_EQUAL("{field}:[* TO {to}]"),
+    GREATER_THAN("{field}:{{from} TO *]"),
+    GREATER_THAN_EQUAL("{field}:[{from} TO *]"),
+    BEFORE("{field}:[* TO {to}}"),
+    AFTER("{field}:{{from} TO *]"),
+    LIKE("{field}:{value}*"),
+    NOT_LIKE("-{field}:{value}*"),
+    STARTING_WITH("{field}:{value}*"),
+    ENDING_WITH("{field}:*{value}"),
+    CONTAINING("{field}:*{value}*"),
+    IN("{field}:({value}... )"),
+    NOT_IN("-{field}:({value}...)"),
+    TRUE("{field}:true"),
+    FALSE("{field}:false");
+
+    public final String label;
+
+    Operators(String label) {
+        this.label = label;
+    }
+}
