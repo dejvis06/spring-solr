7179fa8e3975c6acfb2731b9e7de9f714e831336
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-12 17:12:55 +0100

    test fq build

--- src/test/java/com/example/demo/SolrQueryTests.java
+++ src/test/java/com/example/demo/SolrQueryTests.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
 import com.example.demo.query.SolrQueryBuilder;
-import com.example.demo.query.decorator.components.Facet;
-import com.example.demo.query.decorator.components.FieldList;
-import com.example.demo.query.decorator.components.Pagination;
-import com.example.demo.query.decorator.components.Sort;
-import com.example.demo.query.queries.DefaultSolrQuery;
+import com.example.demo.query.decorator.components.*;
+import com.example.demo.query.operations.Operators;
+import com.example.demo.query.static_.DefaultSolrQuery;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -24,11 +22,24 @@ public class SolrQueryTests {
 
     @Test
     void defaultQueryTest() throws SolrServerException, IOException {
-
         SolrQueryBuilder solrQueryBuilder = new DefaultSolrQuery();
         solrQueryBuilder = new Sort(solrQueryBuilder, "id", SolrQuery.ORDER.desc);
         solrQueryBuilder = new Pagination(solrQueryBuilder, 0, 10);
-        solrQueryBuilder = new FieldList(solrQueryBuilder, "name");
+        solrQueryBuilder = new FieldList(solrQueryBuilder, "id", "name");
+
+        String id_fq = new FilterQuery.FilterQueryBuilder()
+                .operator(Operators.IS)
+                .field("id")
+                .value("96474e52-37c4-4a1a-9734-fcc290620ef8")
+                .build();
+
+        String name_fq = new FilterQuery.FilterQueryBuilder()
+                .operator(Operators.IS)
+                .field("name")
+                .value("Amazon Kindle Paperwhite")
+                .build();
+
+        solrQueryBuilder = new FilterQuery(solrQueryBuilder, id_fq, name_fq);
 
         SolrQuery solrQuery = solrQueryBuilder.build();
 
