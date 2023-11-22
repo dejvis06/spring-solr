86e6d08954eadc9f34c9741e812803961cb9849c
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-22 20:15:45 +0100

    remove unused import

--- src/main/java/com/example/demo/configs/SolrConfigs.java
+++ src/main/java/com/example/demo/configs/SolrConfigs.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Configuration
 public class SolrConfigs {
