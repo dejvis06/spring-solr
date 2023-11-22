7b709432294e66f5a197f67d9c2187b706fae41d
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-22 20:11:35 +0100

    add app runner config for scanning & registering solr repos in context

--- src/main/java/com/example/demo/configs/SolrConfigs.java
+++ src/main/java/com/example/demo/configs/SolrConfigs.java
@@ -3,24 +3,89 @@ package com.example.demo.configs;
 import com.example.demo.MyInvocationHandler;
 import com.example.demo.configs.annotations.SolrRepository;
 import com.example.demo.repositories.SimpleSolrRepository;
-import com.example.demo.repositories.TechProductRepository;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.SystemPropertyUtils;
 
+import java.io.IOException;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
-@ComponentScan(basePackages = "org.example.demo",
-        includeFilters = @Filter(type = FilterType.ANNOTATION, value = SolrRepository.class))
 public class SolrConfigs {
 
-    @Bean
+    /*@Bean
     TechProductRepository techProductRepository() {
         return (TechProductRepository) Proxy.newProxyInstance(getClass().getClassLoader(),
                 new Class[]{TechProductRepository.class},
                 new MyInvocationHandler(new SimpleSolrRepository<>()));
+    }*/
+
+    @Bean
+    ApplicationRunner solrRepositoriesScanner(ConfigurableListableBeanFactory beanFactory) {
+        return (args) -> {
+            for (Class solrRepository : emptyIfNull(findSolrRepositories("com.example.demo"))) {
+                beanFactory.registerSingleton(solrRepository.getSimpleName(), generateProxy(solrRepository));
+            }
+            Map<String, Object> solrRepositories = beanFactory.getBeansWithAnnotation(SolrRepository.class);
+            // solrRepositories.forEach((name, solrRepository) -> beanFactory.registerSingleton(name, generateProxy(solrRepository)));
+        };
+    }
+
+    private static List<Class> emptyIfNull(List<Class> solrRepositories) {
+        if (solrRepositories != null)
+            return solrRepositories;
+        return new ArrayList<>();
+    }
+
+    private Object generateProxy(Class clazz) {
+        return Proxy.newProxyInstance(getClass().getClassLoader(),
+                new Class[]{(clazz)},
+                new MyInvocationHandler(new SimpleSolrRepository<>()));
+    }
+
+    private List<Class> findSolrRepositories(String basePackage) throws IOException, ClassNotFoundException {
+        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        MetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory(resourcePatternResolver);
+
+        List<Class> candidates = new ArrayList<>();
+        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX +
+                resolveBasePackage(basePackage) + "/" + "**/*.class";
+        Resource[] resources = resourcePatternResolver.getResources(packageSearchPath);
+        for (Resource resource : resources) {
+            if (resource.isReadable()) {
+                MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(resource);
+                if (isSolrRepository(metadataReader)) {
+                    candidates.add(Class.forName(metadataReader.getClassMetadata().getClassName()));
+                }
+            }
+        }
+        return candidates;
+    }
+
+    private String resolveBasePackage(String basePackage) {
+        return ClassUtils.convertClassNameToResourcePath(SystemPropertyUtils.resolvePlaceholders(basePackage));
+    }
+
+    private boolean isSolrRepository(MetadataReader metadataReader) {
+        try {
+            Class c = Class.forName(metadataReader.getClassMetadata().getClassName());
+            if (c.getAnnotation(SolrRepository.class) != null) {
+                return true;
+            }
+        } catch (Throwable e) {
+        }
+        return false;
     }
 }
