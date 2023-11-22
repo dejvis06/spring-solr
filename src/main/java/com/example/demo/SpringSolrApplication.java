50d9e41e13e47598b656ea6a31b6377775d0c7a9
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-22 20:10:56 +0100

    remove unused config

--- src/main/java/com/example/demo/SpringSolrApplication.java
+++ src/main/java/com/example/demo/SpringSolrApplication.java
@@ -2,20 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.ContextStartedEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringSolrApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(SpringSolrApplication.class, args);
-	}
-
-	@EventListener
-	public void handleContextRefreshEvent(ContextStartedEvent ctxStartEvt) {
-		System.out.println("Context Start Event received.");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSolrApplication.class, args);
+    }
 }
