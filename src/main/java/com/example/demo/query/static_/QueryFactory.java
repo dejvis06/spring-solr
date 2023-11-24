b477ead47ea914905d594c70abd2dc069cbbecf6
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-24 10:43:41 +0100

    rename solr repo interface

--- src/main/java/com/example/demo/query/static_/QueryFactory.java
+++ src/main/java/com/example/demo/query/static_/QueryFactory.java
@@ -1,10 +1,10 @@
 package com.example.demo.query.static_;
 
-import com.example.demo.repositories.SolrRepository;
+import com.example.demo.repositories.ISolrRepository;
 
 public abstract class QueryFactory {
 
-    public static SolrRepository returnQuery(String config) {
+    public static ISolrRepository returnQuery(String config) {
         return null;
     }
 }
