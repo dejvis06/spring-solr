90f6ae05d90d049fe7639c40042930b6520ca734
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-24 09:51:10 +0100

    rename solr repo interface

--- src/main/java/com/example/demo/repositories/ISolrRepository.java
+++ src/main/java/com/example/demo/repositories/ISolrRepository.java
@@ -2,6 +2,6 @@ package com.example.demo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface SolrRepository<T, ID> extends CrudRepository<T, ID> {
+public interface ISolrRepository<T, ID> extends CrudRepository<T, ID> {
 
 }
--- src/main/java/com/example/demo/repositories/TechProductRepository.java
+++ src/main/java/com/example/demo/repositories/TechProductRepository.java
@@ -1,13 +1,14 @@
 package com.example.demo.repositories;
 
+import com.example.demo.configs.annotations.SolrRepository;
 import com.example.demo.models.TechProduct;
 import com.example.demo.query.annotations.Query;
 
 import java.util.List;
 import java.util.UUID;
 
-@com.example.demo.configs.annotations.SolrRepository
-public interface TechProductRepository extends SolrRepository<TechProduct, UUID> {
+@SolrRepository
+public interface TechProductRepository extends ISolrRepository<TechProduct, UUID> {
 
     @Query(q = "techproduct")
     List<TechProduct> findAll();
