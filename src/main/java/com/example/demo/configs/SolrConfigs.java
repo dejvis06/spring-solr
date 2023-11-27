1c44faa4e07a144b0edb3982b21bdc7a401a1d59
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-27 11:53:21 +0100

    add solr-client config

--- src/main/java/com/example/demo/configs/SolrConfigs.java
+++ src/main/java/com/example/demo/configs/SolrConfigs.java
@@ -1,8 +1,10 @@
 package com.example.demo.configs;
 
-import com.example.demo.SolrRepositoryInvocationHandler;
+import com.example.demo.repositories.SolrRepositoryInvocationHandler;
 import com.example.demo.configs.annotations.SolrRepository;
-import com.example.demo.repositories.SimpleSolrRepository;
+import com.example.demo.repositories.impl.SimpleSolrRepository;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.Http2SolrClient;
 import org.reflections.Reflections;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.ApplicationRunner;
@@ -15,6 +17,7 @@ import java.util.Set;
 @Configuration
 public class SolrConfigs {
     public static final String BASE_PACKAGE = "com.example.demo";
+    static final String SOLR_URL = "http://localhost:8983/solr";
 
     /*@Bean
     TechProductRepository techProductRepository() {
@@ -22,6 +25,12 @@ public class SolrConfigs {
                 new Class[]{TechProductRepository.class},
                 new MyInvocationHandler(new SimpleSolrRepository<>()));
     }*/
+
+    @Bean
+    SolrClient solrClient() {
+        return new Http2SolrClient.Builder(SOLR_URL)
+                .build();
+    }
 
     @Bean
     ApplicationRunner solrRepositoriesScanner(ConfigurableBeanFactory beanFactory) {
