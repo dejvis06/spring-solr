1212214e53de2c786c95d48b80c1bbdbcf049c68
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-27 11:04:12 +0100

    place page annotation

--- src/main/java/com/example/demo/query/annotations/Query.java
+++ src/main/java/com/example/demo/query/annotations/Query.java
@@ -1,13 +1,12 @@
 package com.example.demo.query.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Query {
 
     String q();
+    Sort sort();
+    Page page();
 }
