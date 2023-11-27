e5ea57ab04b19a8ac560c18560739a4b9c380499
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-27 11:54:44 +0100

    optimize imports

--- src/test/java/com/example/demo/ReflectionTests.java
+++ src/test/java/com/example/demo/ReflectionTests.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.query.decorators.components.PageRequest;
 import com.example.demo.repositories.TechProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
