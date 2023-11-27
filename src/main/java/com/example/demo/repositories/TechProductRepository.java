034522c375295a3455292e5b5cbcc040ce0f6a0f
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-27 11:05:38 +0100

    add new annotation

--- src/main/java/com/example/demo/repositories/TechProductRepository.java
+++ src/main/java/com/example/demo/repositories/TechProductRepository.java
@@ -2,7 +2,11 @@ package com.example.demo.repositories;
 
 import com.example.demo.configs.annotations.SolrRepository;
 import com.example.demo.models.TechProduct;
+import com.example.demo.query.SolrQueryBuilder;
+import com.example.demo.query.annotations.Page;
 import com.example.demo.query.annotations.Query;
+import com.example.demo.query.annotations.Sort;
+import org.apache.solr.client.solrj.SolrQuery;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,6 +14,8 @@ import java.util.UUID;
 @SolrRepository
 public interface TechProductRepository extends ISolrRepository<TechProduct, UUID> {
 
-    @Query(q = "techproduct")
-    List<TechProduct> findAll();
+    @Query(q = "techproduct",
+            sort = @Sort(field = "", order = SolrQuery.ORDER.desc),
+            page = @Page(start = 0, rows = 10))
+    List<TechProduct> findAll(SolrQueryBuilder solrQueryBuilder);
 }
