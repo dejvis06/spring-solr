6a018060f71c187cd7b8ebc6319984cd8b112084
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-28 17:35:05 +0100

    add fieldlist and handle dynamic params

--- src/main/java/com/example/demo/query/QueryParser.java
+++ src/main/java/com/example/demo/query/QueryParser.java
@@ -1,6 +1,8 @@
 package com.example.demo.query;
 
 import com.example.demo.query.annotations.Query;
+import com.example.demo.query.decorators.SolrQueryDecorator;
+import com.example.demo.query.decorators.components.FieldList;
 import com.example.demo.query.decorators.components.PageRequest;
 import com.example.demo.query.decorators.components.QQuery;
 import org.apache.solr.client.solrj.SolrQuery;
@@ -10,10 +12,11 @@ public final class QueryParser {
     public static SolrQuery parse(Query query, SolrQueryBuilder[] params) throws SolrQueryException {
         SolrQueryBuilder solrQueryBuilder = new QQuery(query.q());
         solrQueryBuilder = new PageRequest(solrQueryBuilder, query.sort(), query.page());
+        solrQueryBuilder = new FieldList(solrQueryBuilder, query.fl().selected());
 
         for (SolrQueryBuilder param : handleNullParams(params)) {
             if (param != null) {
-                solrQueryBuilder = param;
+                solrQueryBuilder = ((SolrQueryDecorator) param).setSolrQueryBuilder(solrQueryBuilder);
             }
         }
         return solrQueryBuilder.build();
--- src/main/java/com/example/demo/query/decorators/SolrQueryDecorator.java
+++ src/main/java/com/example/demo/query/decorators/SolrQueryDecorator.java
@@ -5,4 +5,9 @@ import com.example.demo.query.SolrQueryBuilder;
 public abstract class SolrQueryDecorator implements SolrQueryBuilder {
 
     protected SolrQueryBuilder solrQueryBuilder;
+
+    public SolrQueryDecorator setSolrQueryBuilder(SolrQueryBuilder solrQueryBuilder) {
+        this.solrQueryBuilder = solrQueryBuilder;
+        return this;
+    }
 }
