57b91469a9cc1992c8b8c6db55032f0c98282e9d
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-28 10:32:06 +0100

    append params to solr query builder

--- src/main/java/com/example/demo/query/QueryParser.java
+++ src/main/java/com/example/demo/query/QueryParser.java
@@ -5,12 +5,17 @@ import com.example.demo.query.decorators.components.PageRequest;
 import com.example.demo.query.decorators.components.QQuery;
 import org.apache.solr.client.solrj.SolrQuery;
 
+import java.util.Arrays;
+
 public final class QueryParser {
 
     public static SolrQuery parse(Query query, SolrQueryBuilder... params) {
         // TODO: handle args & other parts of the query
         SolrQueryBuilder solrQueryBuilder = new QQuery(query.q());
         solrQueryBuilder = new PageRequest(solrQueryBuilder, query.sort(), query.page());
+        for (SolrQueryBuilder param : Arrays.stream(params).toList()) {
+            solrQueryBuilder = param;
+        }
         return solrQueryBuilder.build();
     }
 }
