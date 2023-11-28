85be32cc5d9856aeeb60737d83bc62e5ed74f852
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-28 18:02:00 +0100

    change class name

--- src/main/java/com/example/demo/query/decorators/components/FacetField.java
+++ src/main/java/com/example/demo/query/decorators/components/FacetField.java
@@ -7,18 +7,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // TODO: This case can be expanded to handle all the cases of the Facets, for the moment only facet-fields
-public class Facet extends SolrQueryDecorator {
+public class FacetField extends SolrQueryDecorator {
 
-    static final Logger logger = LoggerFactory.getLogger(Facet.class);
+    static final Logger logger = LoggerFactory.getLogger(FacetField.class);
 
     private String[] fields;
 
-    public Facet(SolrQueryBuilder solrQueryBuilder, String... fields) {
+    public FacetField(SolrQueryBuilder solrQueryBuilder, String... fields) {
         this.solrQueryBuilder = solrQueryBuilder;
         this.fields = fields;
     }
 
-    public Facet fields(String... fields) {
+    public FacetField fields(String... fields) {
         this.fields = fields;
         return this;
     }
