69fdda460ba85bd6cf8348ba2df4873b2ec79e6d
beqirajd <dejvis.beqiraj@linksmt.it>
2023-11-28 18:11:19 +0100

    add facets annotations

--- src/main/java/com/example/demo/repositories/TechProductRepository.java
+++ src/main/java/com/example/demo/repositories/TechProductRepository.java
@@ -3,10 +3,7 @@ package com.example.demo.repositories;
 import com.example.demo.configs.annotations.SolrRepository;
 import com.example.demo.models.TechProduct;
 import com.example.demo.query.SolrQueryBuilder;
-import com.example.demo.query.annotations.FieldList;
-import com.example.demo.query.annotations.Page;
-import com.example.demo.query.annotations.Query;
-import com.example.demo.query.annotations.Sort;
+import com.example.demo.query.annotations.*;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 
@@ -18,6 +15,10 @@ public interface TechProductRepository extends ISolrRepository<TechProduct, UUID
     @Query(
             fl = @FieldList(selected = {"name", "id"}),
             sort = @Sort(field = "id", order = SolrQuery.ORDER.desc),
-            page = @Page(rows = 5))
+            page = @Page(rows = 5),
+            facet = @Facet(
+                    facetField = @FacetField(selected = "name_ss")
+            )
+    )
     QueryResponse findAll(SolrQueryBuilder... solrQueryBuilder);
 }
