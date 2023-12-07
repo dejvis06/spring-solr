9e1cf715b0a59dfa604a4dce6132e2c4da12cdc8
beqirajd <dejvis.beqiraj@linksmt.it>
2023-12-07 14:54:48 +0100

    add aop

---
+++ src/main/java/com/example/demo/configs/SimpleSolrRepositoryAspect.java
@@ -0,0 +1,20 @@
+package com.example.demo.configs;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+class SimpleSolrRepositoryAspect {
+
+    @Pointcut("within(com.example.demo.repositories..*)")
+    public void inDataAccessLayer() {}
+
+    // @After(value = "execution(* com.example.demo.repositories.impl.SimpleSolrRepository.*(..))")
+    @After("execution(* com.example.demo.repositories.*.*(..))")
+    public void commit() {
+        System.err.println("Entered commit!");
+    }
+}
