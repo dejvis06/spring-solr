7fc836d508fce9cb1d7c3b81ed57f849c7a61bd5
beqirajd <dejvis.beqiraj@linksmt.it>
2023-12-07 14:53:23 +0100

    add impl

--- src/main/java/com/example/demo/repositories/impl/SimpleSolrRepository.java
+++ src/main/java/com/example/demo/repositories/impl/SimpleSolrRepository.java
@@ -1,67 +1,59 @@
 package com.example.demo.repositories.impl;
 
+import com.example.demo.models.SolrResponseRest;
+import com.example.demo.models.TechProduct;
+import com.example.demo.query.SolrQueryBuilder;
 import com.example.demo.repositories.ISolrRepository;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrDocument;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.Optional;
+import java.util.UUID;
 
 public class SimpleSolrRepository<T, ID> implements ISolrRepository<T, ID> {
+
+    private final SolrClient solrClient;
+
+    public SimpleSolrRepository(SolrClient solrClient) {
+        this.solrClient = solrClient;
+    }
+
     @Override
     public <S extends T> S save(S entity) {
-        return null;
+        try {
+            solrClient.addBean(entity);
+            // TODO converters SolrDocument -> <T>
+            return entity;
+        } catch (IOException | SolrServerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public <S extends T> Iterable<S> saveAll(Iterable<S> entities) {
-        return null;
-    }
-
-    @Override
-    public Optional<T> findById(ID id) {
+    public Optional<T> findById(ID id) throws SolrServerException, IOException {
+        SolrDocument solrDocument = solrClient.getById(String.valueOf(id));
+        // TODO converters SolrDocument -> <T>
         return Optional.empty();
     }
 
     @Override
-    public boolean existsById(ID id) {
-        return false;
+    public void deleteById(ID id) throws SolrServerException, IOException {
+        solrClient.deleteById(String.valueOf(id));
     }
 
     @Override
-    public Iterable<T> findAll() {
+    public SolrResponseRest<T> findAll(SolrQueryBuilder... solrQueryBuilder) {
         return null;
     }
 
-    @Override
-    public Iterable<T> findAllById(Iterable<ID> ids) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(ID id) {
-
-    }
-
-    @Override
-    public void delete(T entity) {
-
-    }
-
-    @Override
-    public void deleteAllById(Iterable<? extends ID> ids) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends T> entities) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
+    public static SimpleSolrRepository instantiate(Type parameterizedType, SolrClient solrClient) {
+        if (parameterizedType.equals(TechProduct.class)) {
+            return new SimpleSolrRepository<TechProduct, UUID>(solrClient);
+        } else {
+            return null;
+        }
     }
 }
